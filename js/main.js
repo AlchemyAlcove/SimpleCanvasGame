a8c8b3546bdff18f30d660e3c223ce0ae7b2f1b4
Michael Hoffert <mike.hoffert@gmail.com>
2014-03-12 14:20:58 -0500

    Added AI monster to arena

--- js/main.js
+++ js/main.js
@@ -10,12 +10,24 @@ bgImage.src = "images/canvas_background.png";
 var heroImage = new Image();
 heroImage.src = "images/hero.png";
 
+var monsterImage = new Image();
+monsterImage.src = "images/monster.png";
+
 var hero = {};
 hero.height = 32;
 hero.width = 32;
 hero.speed = 250;
 hero.x = (canvas.width / 2) - (hero.width / 2);
 hero.y = (canvas.height / 2) - (hero.height / 2);
+
+var monster = {};
+monster.height = 32
+monster.width = 30;
+monster.speed = 50;
+monster.x = 100;
+monster.y = 100;
+monster.xDirection = monster.speed;
+monster.yDirection = monster.speed;
 
 var keysDown = {};
 
@@ -40,26 +52,40 @@ var moveHero = function (modifier) {
 	if(39 in keysDown) { // Player holding right
 		hero.x += hero.speed * modifier;
 	}
-
-	if(hero.y < 0) {
-		hero.y = 0;
-	}
-	if(hero.x < 0) {
-		hero.x = 0;
-	}
-	if(hero.y > (canvas.height - hero.height)) {
-		hero.y = canvas.height - hero.height;
-	}
-	if(hero.x > (canvas.width - hero.width)) {
-		hero.x = canvas.width - hero.width;
-	}
+	resetObjectLocation(hero);
 };
 
+var moveMonster = function (modifier) {
+	monster.x += monster.xDirection * modifier;
+	monster.y += monster.yDirection * modifier;
+	resetObjectLocation(monster);
+};
+
+var updateMonsterDirection = function () {
+	monster.xDirection = ((Math.random() < 0.5)? -1 : 1) * Math.random() * monster.speed;
+	monster.yDirection= ((Math.random() < 0.5)? -1 : 1) * Math.random() * monster.speed;
+};
+
+var resetObjectLocation = function(object) {
+	if(object.y < 0) {
+		object.y = 0;
+	}
+	if(object.x < 0) {
+		object.x = 0;
+	}
+	if(object.y > (canvas.height - object.height)) {
+		object.y = canvas.height - object.height;
+	}
+	if(object.x > (canvas.width - object.width)) {
+		object.x = canvas.width - object.width;
+	}
+}
 var drawn_hero = null;
 
 var render = function () {
 	ctx.clearRect (0, 0, canvas.width, canvas.height);
 	ctx.drawImage(bgImage, 0, 0);
+	ctx.drawImage(monsterImage, monster.x, monster.y);
 	ctx.drawImage(heroImage, hero.x, hero.y);
 };
 
@@ -68,6 +94,7 @@ var main = function () {
 	var delta = now - then;
 
 	moveHero(delta / 1000);
+	moveMonster(delta / 1000);
 	render();
 
 	then = now;
@@ -75,3 +102,4 @@ var main = function () {
 
 var then = Date.now();
 setInterval(main, 1);
+setInterval(updateMonsterDirection, 1750);
